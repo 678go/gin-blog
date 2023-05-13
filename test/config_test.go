a225494aa5fc24dcf16e1b5782baa8d43f71cb12
ylinyang <ylinyang1228@gmail.com>
2023-05-13 17:43:33 +0800

    [fix]定义用户结构体

--- test/config_test.go
+++ test/config_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"github.com/goccy/go-json"
 	"golang.org/x/exp/slog"
 	"os"
 	"testing"
@@ -10,4 +11,25 @@ func TestLog(t *testing.T) {
 
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{})))
 
+}
+
+type Status int
+
+const (
+	Running Status = 1
+	Except  Status = 2
+	OffLine Status = 3
+)
+
+func (s Status) MarshalJSON() ([]byte, error) {
+	var str string
+	switch s {
+	case Running:
+		str = "Running"
+	case Except:
+		str = "Except"
+	case OffLine:
+		str = "OffLine"
+	}
+	return json.Marshal(str)
 }
